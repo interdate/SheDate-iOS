929a7c6f3d6904ed7d6d7e01281037f1713df6d4
albert <albert@interdate.co.il>
2018-05-15 16:17:29 +0300

    last fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,92 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from "@angular/platform-browser";
+import {ErrorHandler, NgModule} from "@angular/core";
+import {IonicApp, IonicErrorHandler, IonicModule, Nav, Select} from "ionic-angular";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {StatusBar} from "@ionic-native/status-bar";
+import {Media} from "@ionic-native/media";
+import {File} from "@ionic-native/file";
+import {ApiQuery} from "../library/api-query";
+import {IonicStorageModule} from "@ionic/storage";
+import {HttpModule} from "@angular/http";
+import {Device} from "@ionic-native/device";
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {LoginPage} from "../pages/login/login";
+import {RegisterPageModule} from "../pages/register/register.module";
+import {Camera} from "@ionic-native/camera";
+import {ImagePicker} from "@ionic-native/image-picker";
+import {FileTransfer} from "@ionic-native/file-transfer";
+import {PageModule} from "../pages/page/page.module";
+import {ChangePhotosPageModule} from "../pages/change-photos/change-photos.module";
+import {AdvancedsearchPageModule} from "../pages/advancedsearch/advancedsearch.module";
+import {Geolocation} from "@ionic-native/geolocation";
+import {SettingsPageModule} from "../pages/settings/settings.module";
+import {SubscriptionPageModule} from "../pages/subscription/subscription.module";
+import {AdvancedSearchResultPageModule} from "../pages/advanced-search-result/advanced-search-result.module";
+import {Keyboard} from "@ionic-native/keyboard";
+import {FingerprintAIO} from "@ionic-native/fingerprint-aio";
+import {AdminMessagesPageModule} from "../pages/admin-messages/admin-messages.module";
+import {ProfilePage} from "../pages/profile/profile";
+import {AppVersion} from "@ionic-native/app-version";
+import {InAppPurchase} from "@ionic-native/in-app-purchase";
+import {SelectPage} from "../pages/select/select";
+import {SelectPageModule} from "../pages/select/select.module";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        LoginPage,
+        ApiQuery,
+        ProfilePage
+    ],
+    imports: [
+        BrowserModule,
+       IonicModule.forRoot(MyApp, {
+            menuType: 'overlay',
+            scrollAssist: false,
+            autoFocusAssist: true
+        }),
+        IonicStorageModule.forRoot(),
+        HttpModule,
+        SelectPageModule,
+        RegisterPageModule,
+        PageModule,
+        ChangePhotosPageModule,
+        AdvancedsearchPageModule,
+        AdvancedSearchResultPageModule,
+        SettingsPageModule,
+        SubscriptionPageModule,
+        AdminMessagesPageModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        LoginPage,
+        ProfilePage,
+        SelectPage
+    ],
+    providers: [
+        FingerprintAIO,
+        Nav,
+        Keyboard,
+        StatusBar,
+        SplashScreen,
+/*
+        AndroidFingerprintAuth,
+*/
+        Device,
+        Geolocation,
+        ImagePicker,
+        FileTransfer,
+        InAppPurchase,
+        Camera,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        //{ provide: AndroidFingerprintAuth, useClass: FingerMock },
+        ApiQuery, Media, File, AppVersion
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/pages/subscription/subscription.ts
@@ -0,0 +1,153 @@
+import {Component} from "@angular/core";
+import {NavController, NavParams} from "ionic-angular";
+import {InAppPurchase} from "@ionic-native/in-app-purchase";
+import {ApiQuery} from "../../library/api-query";
+import {Http} from "@angular/http";
+import {HomePage} from "../home/home";
+import {Page} from "../page/page";
+
+/*
+ Generated class for the Subscription page.
+
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
+@Component({
+    selector: 'page-subscription',
+    templateUrl: 'subscription.html',
+    providers: [InAppPurchase]
+})
+export class SubscriptionPage {
+
+    public products: Array<{ productId: any, title: any, price: any, description: any }>;
+    faq: Array<{ q: string, a: string }>;
+    hightlightStatus: Array<boolean> = [];
+    is_showed: Array<boolean> = [];
+    text: any;
+
+    constructor(public navCtrl: NavController, public http: Http, public navParams: NavParams, public api: ApiQuery, private iap: InAppPurchase) {
+
+        this.getRestore();
+
+        this.http.get(this.api.url + '/faq/payment', this.api.header).subscribe(data => {
+            this.faq = data.json().faq;
+            this.text = data.json().text;
+        });
+
+        this.api.showLoad();
+        this.iap
+            .getProducts(['shedate.oneMonth', 'shedate.threeMonths'])
+            .then((products) => {
+                this.products = products;
+                this.api.hideLoad();
+            })
+            .catch((err) => {
+                alert(JSON.stringify(err));
+            });
+
+    }
+
+    subscribe(product) {
+        this.api.showLoad();
+        switch (product.productId) {
+            case 'shedate.oneMonth':
+                var monthsNumber = 1;
+                break;
+
+            case 'shedate.threeMonths':
+                var monthsNumber = 3;
+                break;
+
+            case 'SixMon':
+                var monthsNumber = 6;
+                break;
+
+            case 'OneY':
+                var monthsNumber = 12;
+                break;
+        }
+        this.iap
+            .subscribe(product.productId)
+            .then((data)=> {
+                if (parseInt(data.transactionId) > 0) {
+                    this.http.post(this.api.url + '/user/subscription/monthsNumber:' + monthsNumber, data, this.api.setHeaders(true)).subscribe(data => {
+                        //this.api.presentToast('Congratulations on your purchase of a paid subscription to Dating4Disabled.com', 10000);
+                        this.navCtrl.push(HomePage);
+                    });
+                }
+                this.api.hideLoad();
+            })
+            .catch((err)=> {
+                this.api.hideLoad();
+                alert(JSON.stringify(err));
+            });
+    }
+
+    showed(i, product) {
+        this.is_showed[i] = !this.is_showed[i];
+
+        if (this.is_showed[i] == false) {
+            this.subscribe(product)
+        }
+    }
+
+    sendSubscribe(history) {
+        //alert(JSON.stringify(history));
+        this.http.post(this.api.url + '/user/subscription/restore', history, this.api.setHeaders(true)).subscribe(data => {
+            //alert(JSON.stringify(data.json()));
+            if (data.json().payment == 1) {
+                this.navCtrl.push(HomePage);
+            }
+
+        }, err => {
+            alert(JSON.stringify(err));
+        });
+    }
+
+    getRestore() {
+        var that = this;
+        this.iap.restorePurchases().then(function (data) {
+            //this.restore = data;
+            console.log(data);
+            /*
+             [{
+             transactionId: ...
+             productId: ...
+             state: ...
+             date: ...
+             }]
+             */
+            that.sendSubscribe(data);
+        }).catch(function (err) {
+            alert(JSON.stringify(err));
+        });
+    }
+
+    safeHtml(el): any {
+        if (this.text) {
+            let html = this.text;
+            let div: any = document.createElement('div');
+            div.innerHTML = html;
+            [].forEach.call(div.getElementsByTagName("a"), (a) => {
+                var pageHref = a.getAttribute('click');
+                if (pageHref) {
+                    a.removeAttribute('click');
+                    a.onclick = () => this.getPage(pageHref);
+                }
+            });
+            if (el.innerHTML == '' || typeof el.innerHTML == 'undefined') {
+                el.appendChild(div);
+            }
+        }
+    }
+
+    getPage(id) {
+        this.navCtrl.push(Page, {pageId: id});
+    }
+
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SubscriptionPage');
+    }
+
+}
